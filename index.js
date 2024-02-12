535fdef66a621a5fd1d4297fa5a1199ca668e1b6
Matheus Augusto <125043947+matheusaugustodev@users.noreply.github.com>
2024-02-12 16:00:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,12 +15,8 @@ client.on('ready', () => {
 });
 client.on('message', message => {
 	console.log(`${message.from}: ${message.body}`);
-    if(message.from === '556299105013@c.us' || message.from === '5562986306799@c.us' || message.from === '556286306799@c.us' 
-    || message.from === '556292671567@c.us'
-    || message.from === '5562992671567@c.us') {
-		// client.sendMessage(message.from, 'Eae Pedro');
+	
         message.reply(`Fala chefe! Você me enviou isso: ${message.body}`);
-	}
 });
 
  
